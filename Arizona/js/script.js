a8be5220fa2afb5e892cc31b03275fbeca9aaca0
luccamarshall <52831210+luccamarshall@users.noreply.github.com>
2024-09-04 08:23:00 +0100

    animation for smooth about section transition javascript idea

--- Arizona/js/script.js
+++ Arizona/js/script.js
@@ -24,3 +24,43 @@ $(document).ready(function(){
      };
    };
  });
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Smooth Scroll
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  // Other existing JavaScript code
+  $("#toggler").click(function () {
+    $("#toggle-nav").fadeToggle(500);
+  });
+
+  $('.menu-toggle').on('click', function () {
+    $('.wrapper').toggleClass('menu--is-revealed');
+  });
+
+  var alterClass = function () {
+    var ww = document.body.clientWidth;
+    if (ww < 1199) {
+      $('.LeftContent').removeClass('LeftContent');
+      $('.RightContent').removeClass('RightContent');
+    } else if (ww >= 1200) {
+      $('.LeftContent').addClass('LeftContent');
+      $('.RightContent').addClass('RightContent');
+    }
+  };
+
+  $(window).resize(function () {
+    alterClass();
+  });
+
+  // Trigger the function on page load
+  alterClass();
+});
