9bbe24149ed1cd48f9b4fd205594380904df8f7a
luccamarshall <52831210+luccamarshall@users.noreply.github.com>
2024-09-09 15:15:42 +0100

    seperated js and html files for design page animations

--- Arizona/js/script.js
+++ Arizona/js/script.js
@@ -42,3 +42,38 @@ $(document).ready(function () {
   // Trigger the function on page load
   alterClass();
 });
+
+// Swiper settings
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
